4cfe4601bb88a84a6c5b680e96ff3ccc705d051f
Hung <hung.nguyen@bionova.fi>
2021-10-11 19:22:30 +0300

    day-1

---
+++ 1-introduction/day-1.py
@@ -0,0 +1,46 @@
+# Interpreted language (python), compiled language (java)
+
+# # IDE
+# print("hello")
+#
+# # default methods / function
+# # print("Tien")
+# # print("Hieu")
+# # print("Tien")
+# # print("Hieu")
+#
+# # 1 variable - 1 value (multiple data types)
+# name_tien = "Tran Thuy Tien"
+# name_hieu = "Hieu"
+# print(name_hieu)
+# print(name_tien)
+
+# data types
+# numeric value (integer, float, complex)
+# number = 1
+# # text, String
+# text = "1"
+#
+# # boolean
+# # true / false
+#
+#
+# print(type(number))
+# print(type(text))
+
+# tu duy lap trinh
+# syntax (cu phap)
+# logic
+
+# reuse variable
+
+computer = "Asus"
+print(computer)
+
+computer = "Dell"
+print(computer)
+
+computer = 1
+print(computer)
+
+# loosely typed - strongly typed
