3206f543e081f223f559a3fccf934604af67f843
Hung <hung.nguyen@oneclicklca.com>
2021-10-17 12:43:32 +0300

    Create day-3.py

---
+++ 1-introduction/day-3.py
@@ -0,0 +1,30 @@
+
+#maintainable
+tien = "Tien tran"
+swimming = "do something"
+print(tien)
+print(swimming)
+print("not good")
+print(tien)
+print('do something')
+print('good')
+
+tien_family = "tien tran, ba tien tran"
+
+print("Tien tran")
+
+# DRY - DON'T REPEAT YOURSELF
+
+"""
+unit test
+    functions
+
+integration test
+    
+functional - QA test (quality assurance) 
+    manual
+    automated
+---
+
+UAT - Unit 
+"""
