78d9d68404de7c03f79e919a01ee046d716cf94b
Hung <happyhung@yahoo.com>
2021-10-22 00:15:43 +0300

    Create assignment-2.py

---
+++ 1-introduction/assignment-2.py
@@ -0,0 +1,24 @@
+"""
+Task: the student grading program
+    Hung is teaching a small class and they just had a test.
+    He has already graded the papers on the scale of 100 but in Finland grades are on scale of 5.
+    Can you write a program that helps him convert the grade to the Finnish system?
+    The program should ask him for his inputs.
+    Try to write the program using functions (define your own function and use it)
+
+Extras:
+    - If the grade is equal or less than 2, the software would ask Hung whether he wants to call the parent.
+    - The software lets him input a list of grades and will tell him the proportion in his class that has grades < 3.
+    - His class have 10 students and he would like to know the top 5 grades. (Hung inputs 10 grades, sw tells the best 5)
+
+Ultimate extras:
+    - Can the software let him input not only grades but also student names? After inputting the name, he would like to check the grades
+    of some students by name as well.
+      [
+        The complexity of this feature is entirely up to you. 
+        If you would like the software to work non-stop, look for while loop. Hint: "while True" or "while condition"
+      ]
+    - He teaches 2 classes, can the software allow him to input the grades for both classes and print it all out at the end when he types "finish"?
+      The print out should  print each class separately with class number / name, and list of students their grades in that class.
+      If you can, print out the list of students in alphabetical order. 
+"""
