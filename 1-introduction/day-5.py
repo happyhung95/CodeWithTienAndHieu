eff0e8905ffc7ef94801672f3581089ac1ee5d13
Hung <hung.nguyen@oneclicklca.com>
2021-10-30 01:37:18 +0300

    day 5

---
+++ 1-introduction/day-5.py
@@ -0,0 +1,104 @@
+# operators
+"""
+#Arithmetic
+5 / 2 = 2.5
+5 / 2 = 2 * 2 + 1
+
+5 % 2 = 1
+5 // 2 = 2
+
+7 % 3 = 1
+7 // 3 = 2
+
+10 % 3 = 1
+10 // 3 = 3
+"""
+
+# assignment
+x = 5
+x = x + 1
+x += 1
+
+y = '2'
+y += '3'
+# concatenate
+
+# Comparison
+a = 1
+b = 2
+
+hung = 'hung'
+hung1 = 'hung'
+tien = 'tien'
+
+list_hung = [1,2,4]
+list_tien = [1,2,3]
+
+# print(list_hung != list_tien)
+# print(list_hung == list_tien)
+
+x = 5
+
+x != 3
+
+# print(x < 2) # False
+# print(x < 10) # True
+# print(x < 2 and x < 10)
+
+# True and True => True
+# True and False => False
+# False and True => False
+# False and False => False
+
+# True or True => True
+# True or False => True
+# False or True => True
+# False or False => False
+
+# compare = (2 < 1 and 1 > 2) or 2 > 1 or 3 > 5
+# print(compare)
+
+# Object
+
+# pointer
+x = '1'
+y = '1'
+# print('x is y', x is y)
+# print('x == y', x == y)
+
+# print(1 in list_hung)
+list_hung = [1,2]
+list_tien = [2]
+# print('list_hung is list_tien',list_hung is list_tien)
+# print('list_hung == list_tien',list_hung == list_tien)
+
+# RAM - Memory
+# CPU - RAM / Memory - Disk
+
+tien = 'tien'
+tran = 'tran'
+
+# if tien == tran:
+#     print('tien is tran')
+#     print('lala')
+# else:
+#     print('tien is not tran')
+#
+# if tien != tran:
+#     print('Tien khong phai la Tran')
+
+# for loop / while loop
+
+# for x in [1,2,3]:
+#     print('hello')
+#     print(x)
+#     print('goodbye')
+
+condition = True
+while condition:
+    print('1')
+    condition = False
+
+
+
+
